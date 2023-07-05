102429cdec11f019c4007c3799228f4a08bf3ae6
alexchun78 <alexchun78@gmail.com>
2023-07-05 15:54:04 +0900

    app 환경 함수화

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,51 +1,77 @@
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
 import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+//enum for states
+enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 }
 
 class App {
+  private _canvas : HTMLCanvasElement;
+  private _scene : Scene;
+  private _engine : Engine;
 
-    private _canvas : HTMLCanvasElement;
-    private _scene : Scene;
-    private _engine : Engine;
+  private _state : State = State.START;// private _state : number = 0;
 
-    constructor() {
+  constructor() {
 
-        this._canvas = this._createCanvas();
+    this._canvas = this._createCanvas();
 
-        // initialize babylon scene and engine
-        this._engine = new Engine(this._canvas, true);
-        this._scene = new Scene(this._engine);
+    // initialize babylon scene and engine
+    this._engine = new Engine(this._canvas, true);
+    this._scene = new Scene(this._engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
-        camera.attachControl(this._canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+    var camera = this._setupCamera();
+    var light1 = this._setupLight();
+    var model = this._setupModel();
+    
+    this._addEvent();
+    this._render();
+  }
 
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'i') {
-                if (this._scene.debugLayer.isVisible()) {
-                    this._scene.debugLayer.hide();
-                } else {
-                    this._scene.debugLayer.show();
-                }
-            }
-        });
+  private _createCanvas(): HTMLCanvasElement {
+    const canvas = document.createElement("canvas");
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.id = "gameCanvas";
+    document.body.appendChild(canvas);
+    return canvas;
+  }
 
-        // run the main render loop
-        this._engine.runRenderLoop(() => {
-            this._scene.render();
-        });
-    }
+  private _setupCamera():ArcRotateCamera{
+    const camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
+    camera.attachControl(this._canvas, true);
+    return camera;
+  }
 
-    private _createCanvas(): HTMLCanvasElement {
-        var canvas = document.createElement("canvas");
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.id = "gameCanvas";
-        document.body.appendChild(canvas);
-        return canvas;
-    }
+  private _setupLight():HemisphericLight{
+    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
+    return light1;
+  }
+
+  private _setupModel():Mesh{
+    var model: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+    return model;
+  }
+
+  private _addEvent():void{
+    // hide/show the Inspector
+    window.addEventListener("keydown", (ev) => {
+      // Shift+Ctrl+Alt+I
+      console.log(ev.keyCode);
+      if (ev.shiftKey && ev.ctrlKey && ev.altKey && (ev.key === 'i' || ev.key === 'I')) {
+        if (this._scene.debugLayer.isVisible()) {
+          this._scene.debugLayer.hide();
+        } else {
+          this._scene.debugLayer.show();
+        }
+      }
+    });
+  }
+
+  private _render():void{
+    // run the main render loop
+    this._engine.runRenderLoop(() => {
+      this._scene.render();
+    });
+  }
 }
 new App();
